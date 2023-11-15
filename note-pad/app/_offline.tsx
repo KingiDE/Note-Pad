b139b9440ff22f89034b9f97ce36c72b1fb22170
Kingi <121757014+KingiDE@users.noreply.github.com>
2023-11-15 19:21:57 +0100

    test commit

---
+++ note-pad/app/_offline.tsx
@@ -0,0 +1,10 @@
+const Test = () => {
+  return (
+    <div>
+      <div>You are offline</div>
+      <p>Now</p>
+    </div>
+  );
+};
+
+export default Test;
--- note-pad/next.config.js
+++ note-pad/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
   // eslint-disable-next-line no-undef
-  disable: process.env.NODE_ENV === 'development',
+  // disable: process.env.NODE_ENV === 'development',
 });
 
 // eslint-disable-next-line no-undef
